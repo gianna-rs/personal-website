bc40e7cd5fd34fea53130643ded828638dea9083
Gianna <giannamread@gmail.com>
2020-12-11 19:24:36 -0700

    Set up boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("home")
+});
+
+app.listen(3000, () =>{
+    console.log("Connected to port 3000")
+});
